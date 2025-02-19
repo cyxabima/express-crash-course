e2ff7e4fa48323a30447769e8cb071b4af28bb42
cyxabima <ukashaanwerali@gmail.com>
2025-02-19 21:02:14 +0500

    req params Done

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const path = require('path')
+require("dotenv").config()
 
 const app = express();
-const port = 4090;
+const port = process.env.PORT || 8090;
 
 // setup static folder
-app.use(express.static(path.join(__dirname,"public")))
+// app.use(express.static(path.join(__dirname,"public")))
+
+let post = [
+    {
+        id: 1, title: "post1"
+    },
+    {
+        id: 2, title: "post2"
+    },
+    {
+        id: 3, title: "post3"
+    },
+];
+
+// get all post
+app.get("/api/posts", (req, res) => {
+    res.json(post)
+})
+
+// get single post 
+
+app.get("/api/posts/:id", (req, res)=>{
+    const id = parseInt(req.params.id)
+    res.json(post.filter((post)=> post.id == id))
+})
 
 
 
 
-app.listen(port, ()=> console.log("server is listening on", port));
+app.listen(port, () => console.log("server is listening on", port));
