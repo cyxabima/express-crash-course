7a435a9d9b621d00adf4a256e172c22afdac35d0
cyxabima <ukashaanwerali@gmail.com>
2025-02-20 17:52:47 +0500

    full-curd-api-done

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -69,5 +69,16 @@ router.put("/:id", (req, res) => {
     res.status(200).json(posts)
 })
 
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post =  posts.find((post)=> post.id === id)
+
+    if (!post) {
+        return res.status(404).json({ msg: "Post not found" })
+    }
+    posts = posts.filter((post) => post.id !== id)
+    res.status(200).json(posts)
+})
+
 
 export default router
