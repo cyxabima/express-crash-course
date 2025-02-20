0173bc1d49af38fb61d2c5dd02b2c0ae20fc6e23
cyxabima <ukashaanwerali@gmail.com>
2025-02-20 20:57:18 +0500

    colors-requests

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,18 @@
+import colors from "colors"
+
 const logger = (req, res, next) => {
+    const methodColors = {
+        GET: "green",
+        POST: "blue",
+        PUT: "yellow",
+        DELETE: "red"
+    }
+
+    const color = methodColors[req.method] || "white"
     console.log(
-        `${req.method} ${req.protocol}://${1}${req.hostname}/${req.originalUrl}
-        `)
-        next()
+        `${req.method} ${req.protocol}://${req.hostname}/${req.originalUrl}`[color]
+        )
+    next()
 }
 
 
