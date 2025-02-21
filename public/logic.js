26dbec2a5bb6e61a95b8e6e1c2529877a4bbb80d
cyxabima <ukashaanwerali@gmail.com>
2025-02-21 15:32:36 +0500

    Post request from frontend

--- public/logic.js
+++ public/logic.js
@@ -2,6 +2,7 @@
 
 const button = document.querySelector("#get-post-button")
 const output = document.querySelector("#output")
+const form =  document.querySelector("#form-add-post")
 
 const getPost = async () => {
     output.innerHTML = ""
@@ -23,5 +24,71 @@ const getPost = async () => {
     }
 }
 
+async function addPost(e) {
+    e.preventDefault()
+
+        const formData = new FormData(this)
+        const title  = formData.get('title');
+        try {
+            const response =  await fetch("http://localhost:7080/api/posts",
+                {
+                    method : "POST",
+                    headers : {
+                        "content-type" : 'application/json'
+                    },
+                    body : JSON.stringify({title})
+                }
+            );
+
+            if(!response.ok){
+                throw new Error("Post failed")
+            }
+        
+            const newPost  = await response.json();
+
+            const postEl = document.createElement("div");
+            postEl.textContent =  newPost.title;
+            getPost()
+            output.appendChild(postEl)
+        } catch (error) {
+            console.error("an error occur", error)
+        }
+
+    
+}
 
 button.addEventListener('click', getPost)
+
+form.addEventListener('submit', addPost)
+
+
+
+// async function addPost(e) {
+//     e.preventDefault()
+
+//     const formData = new FormData(this)
+//     const title  = formData.get('title');
+
+//     fetch('http://localhost:7080/posts', {
+//         method: "POST", 
+//         headers : {
+//             "Content-Type " : "application/json"
+//         },
+//         body : JSON.stringify({title})
+//     }).then((response)=>{
+//         newPost = response.json()
+
+//         const postEl = document.createElement("div")
+//         postEl.textContent = newPost.title
+//         getPost()
+//         output.appendChild(postEl)
+        
+
+
+//     }).catch((error)=>{
+//         console.error("error adding post ", error)
+//     })
+
+
+
+// }
